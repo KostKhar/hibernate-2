a9bf82e0ff22bcc01e9b37279315e8096085634c
kostkhar <konstantin.kharik@onlinesup.com>
2025-11-21 21:16:41 +0300

    add film_category

--- src/main/java/entity/FilmCategory.java
+++ src/main/java/entity/FilmCategory.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
 
 @Entity
 @Data
@@ -13,5 +16,18 @@ import lombok.ToString;
 @ToString
 @Table(name = "film_category", schema = "movie")
 public class FilmCategory {
+    @Id
+    @Column(name = "film_id")
+    private int film_id;
+
+
+    @Column(name = "category_id")
+    private int category_id;
+
+
+    @Column(name = "last_update", nullable = false)
+    @UpdateTimestamp
+    private Date lastUpdate;
+
 
 }
